64425f5c7a90e1f8189478454ede681032e828d9
Carlos Miceli <carlosmiceli87@gmail.com>
2022-01-30 13:15:19 -0300

    clean up files

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,22 +1,29 @@
-import React from 'react'
-import './style.css'
+import React from "react";
+import "./style.css";
 
 const Footer = () => {
-    return (
-        <div className="footer-cont">
-            <div className="footer-text">
-                <h4>Contact Us</h4>
-                <a href="mailto:hello@estoniaexp.com" target="_blank" >hello@estoniaexp.com</a>
-                <br />
-                <p>Estonia Experience © 2018-2021</p>
-                <p className="footer-author">Website built by <a href="https://github.com/carlosmiceli">Charly</a></p>
-            </div>
-            {/* <div className="footer-social">
+	return (
+		<div className="footer-cont">
+			<div className="footer-text">
+				<h4>Contact Us</h4>
+				<a href="mailto:hello@estoniaexp.com" target="_blank" rel="noreferrer">
+					hello@estoniaexp.com
+				</a>
+				<br />
+				<p>Estonia Experience © 2018-2022</p>
+				<p className="footer-author">
+					Website built by{" "}
+					<a href="https://carlosmiceli.com/" target="_blank" rel="noreferrer">
+						Charly
+					</a>
+				</p>
+			</div>
+			{/* <div className="footer-social">
                 <h4>Follow Us</h4>
                 <a href="https://www.instagram.com/p/BxhU2YKAHd8/" target="_blank" ><img alt="" src="https://firebasestorage.googleapis.com/v0/b/estonia-experience.appspot.com/o/icons%2Finstagram.png?alt=media&token=413be086-bfe4-4b94-925d-28ed7dd54481" /></a>
             </div> */}
-        </div>
-    )
-}
+		</div>
+	);
+};
 
-export default Footer
+export default Footer;
